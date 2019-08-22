bdc45c6be4dbc78954047228f9653289e756778a
Emil Andersson <emil.andersson@apsis.com>
2019-08-22 14:00:19 +0200

    1.0.4

--- tslint.js
+++ tslint.js
@@ -1,15 +1,12 @@
 module.exports = {
-  "rules": {
-    "directive-selector": [true, "attribute", , "camelCase"],
-    "component-selector": [true, "element", , "kebab-case"],
-    "use-input-property-decorator": true,
-    "use-output-property-decorator": true,
-    "use-host-property-decorator": true,
-    "no-input-rename": true,
-    "no-output-rename": true,
-    "use-life-cycle-interface": true,
-    "use-pipe-transform-interface": true,
-    "component-class-suffix": true,
-    "directive-class-suffix": true
-  }
+  rules: {
+    'directive-selector': [true, 'attribute', , 'camelCase'],
+    'component-selector': [true, 'element', , 'kebab-case'],
+    'no-input-rename': true,
+    'no-output-rename': true,
+    'use-life-cycle-interface': true,
+    'use-pipe-transform-interface': true,
+    'component-class-suffix': true,
+    'directive-class-suffix': true,
+  },
 };
